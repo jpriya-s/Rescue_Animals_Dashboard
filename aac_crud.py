b7c409ef69c63ca3f7184f7137d47ea4f184f8f1
jpriya-s <142194396+jpriya-s@users.noreply.github.com>
2024-08-25 15:46:39 -0400

    Update aac_crud.py

--- aac_crud.py
+++ aac_crud.py
@@ -17,19 +17,36 @@ class AnimalShelterCrud:
    
     def create(self, data):
       ''' Method to insert animal record '''
-        try:
-          results = self.collection.insert_one(data)
-          return True if results.inserted_id else False
-        except Exception as e:
-          print(f"Error inserting document: {e}")
-          return False
+      try:
+        results = self.collection.insert_one(data)
+        return True if results.inserted_id else False
+      except Exception as e:
+        print(f"Error in inserting document: {e}")
+        return False
         
     def read(self, query):
       ''' Method to read animal record '''
-        try:
-          cursor = self.collection.find(query)
-          return list(cursor)
-        except Exception as e:
-          print(f"Error quering documents: {e}")
-          return []
-    
+      try:
+        cursor = self.collection.find(query)
+        return list(cursor)
+      except Exception as e:
+        print(f"Error quering documents: {e}")
+        return []
+        
+    def update(self, query, new_data):
+      ''' Method to update animal records '''  
+      try:
+        result = self.collection.update_many(query, {'$set': new_data})
+        return result.modified_count
+      except Exception as e:
+        print(f"An error occured in update operation: {e}")
+        return 0
+        
+    def delete(self, query):
+      ''' Method to update animal records '''  
+      try:
+        result = self.collection.delete_many(query)
+        return result.deleted_count
+      except Exception as e:
+        print(f"An error occured in delete operation: {e}")
+        return 0
